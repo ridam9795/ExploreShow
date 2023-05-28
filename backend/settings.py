20146d3b4a6c43c64d9c8740a91a8b3d4cd3b478
Ridam Nagar <ridamnagar11@gmail.com>
2023-05-28 16:20:29 +0530

    Modified python version

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3i06#os768(oa9@8$-wh%e_a)88mowtshi2@ufr9u(=-b5xv%!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','localhost']
 from datetime import timedelta
 
 
